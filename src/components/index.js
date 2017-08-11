f942a8afee3095ff7bb9ca6a58e6ac4cb89e6c7a
Jack Franklin <jack@jackfranklin.net>
2017-08-11 09:28:20 +0100

    Set up build

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import CheckboxInput from './checkbox-input'
+import Input from './input'
+import SelectInput from './select-input'
+import TextInput from './text-input'
+import Form from './form'
+
+export { CheckboxInput, Input, SelectInput, TextInput, Form }
